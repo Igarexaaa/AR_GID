39843204d9439e9faa5991b8bbfcaa891f2ede25
Igarexaaa <48678361+Igarexaaa@users.noreply.github.com>
2020-06-22 23:47:14 +0900

    namana

--- Assets/Scripts/Glavnoemenu.cs
+++ Assets/Scripts/Glavnoemenu.cs
@@ -5,69 +5,133 @@ using UnityEngine;
 public class Glavnoemenu : MonoBehaviour
 {
 
-    public GameObject ButtonsMenu;
+    public GameObject GlMenu;
+    public GameObject GGlMenuEn;
     public GameObject ARGID;
+    public GameObject AARGIDEn;
     public GameObject Amurgosy;
-    public GameObject Settings;
+    public GameObject AAmurgosyEn;
+    public GameObject En;
+    public GameObject Ru;
+    public GameObject Chi;
     public GameObject Profile;
+    public GameObject amursu;
 
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ShowARGID()
     {
-        ButtonsMenu.SetActive(false);
+        GlMenu.SetActive(false);
         ARGID.SetActive(true);
 
     }
+    public void ShowARGIDEN()
+    {
+        GGlMenuEn.SetActive(false);
+        AARGIDEn.SetActive(true);
 
+    }
 
-    //public void PlanNavig ()
-    //{
-    //    PlanNavig.SetActive(true)
+    public void GlmenuRu()
+    {
+        GlMenu.SetActive(true);
+        En.SetActive(true);
+        GGlMenuEn.SetActive(false);
+        ARGID.SetActive(false);
+        Amurgosy.SetActive(false);        
+        Ru.SetActive(false);
+        Chi.SetActive(false);
+        Profile.SetActive(false);
+        GGlMenuEn.SetActive(false);
+        AARGIDEn.SetActive(false);
+        AAmurgosyEn.SetActive(false);
 
-    //}
+    }
+
+    public void GlmenuEn()
+    {
+        GGlMenuEn.SetActive(true);
+        GlMenu.SetActive(false);               
+        ARGID.SetActive(false);
+        Amurgosy.SetActive(false);
+        En.SetActive(false);
+        Ru.SetActive(true);
+        Chi.SetActive(true);
+        Profile.SetActive(false);        
+        AARGIDEn.SetActive(false);
+        AAmurgosyEn.SetActive(false);
+    }
+
 
     public void ShowAMUR()
     {
-        ButtonsMenu.SetActive(false);
+        GlMenu.SetActive(false);
         Amurgosy.SetActive(true);
 
     }
-
-    public void OpenURLMap()
+    public void ShowAMUREN()
     {
-        Application.OpenURL ("https://yandex.ru/maps/77/blagoveshchensk/?from=api-maps&ll=127.511175%2C50.300556&mode=usermaps&origin=jsapi_2_1_76&um=constructor%3ABcJaZlE8DRmYSfvjID2tcmgHwkNux3_r&z=16");
+        GGlMenuEn.SetActive(false);
+        AAmurgosyEn.SetActive(true);
+
     }
 
-    public void OpenURLEvents()
+    public void OpenURLAmursu()
     {
-        Application.OpenURL("https://www.amursu.ru/events/");
+        Application.OpenURL("https://www.amursu.ru/");
     }
-
-    
 
     public void OpenURLHistory()
-    {
-        Application.OpenURL("https://www.amursu.ru/about/istoriya-universiteta/");
-    }
+                {
+                    Application.OpenURL("https://www.amursu.ru/about/istoriya-universiteta/");
+                }
+                public void OpenURLMesto()
+                {
+                    Application.OpenURL ("https://yandex.ru/maps/77/blagoveshchensk/?from=api-maps&ll=127.511175%2C50.300556&mode=usermaps&origin=jsapi_2_1_76&um=constructor%3ABcJaZlE8DRmYSfvjID2tcmgHwkNux3_r&z=16");
+                }
+
+                public void OpenURLEvent()
+                {
+                    Application.OpenURL("https://www.amursu.ru/events/");
+                }
+
+    
+                public void OpenURLHistoryen()
+                {
+                    Application.OpenURL("https://translate.google.com/translate?hl=&sl=ru&tl=en&u=https%3A%2F%2Fwww.amursu.ru%2Fabout%2Fistoriya-universiteta%2F&sandbox=1");
+                }
+                public void OpenURLMestoen()
+                {
+                    Application.OpenURL("https://yandex.com/maps/77/blagoveshchensk/?from=api-maps&ll=127.511175%2C50.300556&mode=usermaps&origin=jsapi_2_1_76&um=constructor%3ABcJaZlE8DRmYSfvjID2tcmgHwkNux3_r&z=16");
+                }
+
+                public void OpenURLEventen()
+                {
+                    Application.OpenURL("https://translate.google.com/translate?hl=&sl=ru&tl=en&u=https%3A%2F%2Fwww.amursu.ru%2Fevents%2F");
+                }
+
 
     public void BackMenu()
     {
-        ButtonsMenu.SetActive(true);
+        GlMenu.SetActive(true);
         ARGID.SetActive(false);
         Amurgosy.SetActive(false);
-        Settings.SetActive(false);
         Profile.SetActive(false);
+    }
+    public void BackMenuEN()
+    {
+        GGlMenuEn.SetActive(true);
+        GlMenu.SetActive(false);
+        AARGIDEn.SetActive(false);
+        ARGID.SetActive(false);
+        AAmurgosyEn.SetActive(false);
+        Profile.SetActive(false);
+    }
 
+
+    public void Exit ()
+    {
+        Application.Quit();
     }
 
 }
